5b8ef5f4d5d7e5f93e840322d5a95c5e00f8a51a
Victor Klein de Sousa <77276055+VKleinSousa@users.noreply.github.com>
2025-06-12 09:52:56 +0200

    Update setup.py
    added validation command. 
    added usalign requirement

--- setup.py
+++ setup.py
@@ -2,8 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='RBPseg',
-    version='1.1',
-
+    version='1.1.1',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
@@ -17,21 +16,24 @@ setup(
         'joblib',
     ],
     extras_require={
-        'pdbfixer': ['pdbfixer'],  # Recommend users install pdbfixer via conda
+        'pdbfixer': ['pdbfixer'],# Recommend users install via conda
+        'foldseek' : ['foldseek'],  #required for classify module
+        'usalign': ['usalign'], #required for merging module
     },
     entry_points={
         'console_scripts': [
             'rbpseg-sdp=rbpseg.sdp.main:main',
             'rbpseg-merge=rbpseg.merge.merge_main:main',
             'rbpseg-classify=rbpseg.classify:main',
-            'rbpseg-strclust=rbpseg.strclust.strclust:main'
+            'rbpseg-strclust=rbpseg.strclust.strclust:main',
+            'rbpseg-prepare=rbpseg.merge.prepare_files_for_merge:main',
+            'rbpseg-validate=rbpseg.validate.validate_main:main'
         ],
     },
-  
     python_requires='==3.9.21',  # Define Python version compatibility
     author='Victor Klein-Sousa',
-    description='A package for tail fiber structure merging and SDP analysis.',
-    long_description=open('README.md').read(), 
+    description='A package for tail fiber structure analysis.',
+    long_description=open('README.md').read(),  # Make sure this file exists in the root directory
     long_description_content_type='text/markdown',
     url='https://github.com/VKleinSousa/RBPseg',
     classifiers=[
